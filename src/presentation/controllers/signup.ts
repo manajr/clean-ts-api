408e3dd9f51130a63248448fc48521e200f3527f
Manassés Pinto Silva Júnior <mpsj@LenovoS145.(none)>
2021-03-14 17:30:48 -0300

    refactor: move all protocols to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,6 @@
-import { HttpRequest, HttpResponse } from '../protocols/http'
-import { MissingParamError, InvalidParamError } from '../errors/index'
+import { HttpRequest, HttpResponse, EmailValidator, Controller } from '../protocols'
+import { MissingParamError, InvalidParamError } from '../errors'
 import { badRequest, serverError } from '../helper/http-helper'
-import { Controller } from '../protocols/controller'
-import { EmailValidator } from '../protocols/email-validator'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
