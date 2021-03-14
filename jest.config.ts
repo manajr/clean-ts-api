d51341d185ad589560f56a8f95350cb17cd1526d
Manassés Pinto Silva Júnior <mpsj@LenovoS145.(none)>
2021-03-14 10:21:20 -0300

    chore: edit jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,6 @@
 export default {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
