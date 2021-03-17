8b0fba560316745e6d4436f9c774bf566614977f
Manassés Pinto Silva Júnior <mpsj@LenovoS145.(none)>
2021-03-16 21:34:05 -0300

    refactor: remove SutTypes from bcrypt adapter test

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -9,31 +9,27 @@ jest.mock('bcrypt', () => ({
 
 const salt = 12
 
-interface SutTypes {
-  sut: BcryptAdapter
-}
-
-const makeSut = (): SutTypes => {
+const makeSut = (): BcryptAdapter => {
   const sut = new BcryptAdapter(salt)
-  return { sut }
+  return sut
 }
 
 describe('Bcrypt Adapter', () => {
   test('should call bcrypt with correct value', async () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const spyEncrypt = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(spyEncrypt).toHaveBeenCalledWith('any_value', salt)
   })
 
   test('should return a hash on success', async () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hash')
   })
 
   test('should throws when bcrypt throws', async () => {
-    const { sut } = makeSut()
+    const sut = makeSut()
     jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const promise = sut.encrypt('any_value')
     await expect(promise).rejects.toThrow()
