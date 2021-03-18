fd91aecba34dce87e657ae7474c039578b4eb0cf
Manassés Pinto Silva Júnior <mpsj@LenovoS145.(none)>
2021-03-18 20:31:31 -0300

    feat: move mongodb methods to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+  async disconnect () {
+    await this.client.close()
+  }
+}
