ad356194dee274ed00b6829913225a10ffa1bf19
Manassés Pinto Silva Júnior <mpsj@LenovoS145.(none)>
2021-03-18 22:08:45 -0300

    fix: fix bug when execute local database test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
