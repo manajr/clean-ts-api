6e0dc7a31c7c951055b6a41f6f98368940bbb4cb
Manassés Pinto Silva Júnior <mpsj@LenovoS145.(none)>
2021-03-20 08:50:21 -0300

    refactor: improve the readability of route dinamical import

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,5 +4,8 @@ import fg from 'fast-glob'
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  fg.sync('**/src/main/routes/**routes.ts').map(async file => (await import(`../../../${file}`)).default(router))
+  fg.sync('**/src/main/routes/**routes.ts').map(async file => {
+    const route = (await import(`../../../${file}`)).default
+    route(router)
+  })
 }
