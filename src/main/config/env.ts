b4a1c54191339c838fbf78e04616f6024726300f
Manassés Pinto Silva Júnior <mpsj@LenovoS145.(none)>
2021-03-20 14:42:15 -0300

    refactor: change the URL enviromental variable to connect mongoDB

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,12 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const mongoUrl = process.env.NODE_ENV === 'test'
+  ? process.env.MONGO_URL ?? 'mongodb://localhost:27017/tempNode'
+  : process.env.PRODUCTION_MONGODB_URL
+
 export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/tempNode',
+  mongoUrl: mongoUrl,
   port: process.env.PORT ?? 5050
 }
